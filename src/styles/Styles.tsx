269684674b260e0d1de9c08816f3791e707925fb
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-14 23:01:21 +0100

    Add project structure

---
+++ src/styles/Styles.tsx
@@ -0,0 +1,8 @@
+type Colors = 'black' | 'white' | 'grey' | 'darkGrey';
+
+export const Colors: Record<Colors, string> = {
+  black: '#000000',
+  white: '#ffffff',
+  grey: '#7D86A9',
+  darkGrey: '#59607a',
+};
