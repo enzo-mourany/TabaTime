58228c69b213f0cdce83b56988b40686eb602993
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-15 13:52:52 +0100

    feat: add timers lists

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,19 @@
 import React, {useRef} from 'react';
-import {SafeAreaView, StatusBar, Animated} from 'react-native';
+import {SafeAreaView, StatusBar, Animated, Dimensions} from 'react-native';
+
+const {width, height} = Dimensions.get('window');
+
+let timersExercises = [...Array(20).keys()].map((i: number) =>
+  i === 0 ? 1 : i * 5,
+);
+timersExercises.splice(0, 1);
+let timersRest = [...Array(13).keys()].map((i: number) =>
+  i === 0 ? 1 : i * 5,
+);
+timersRest.splice(0, 1);
+
+const item_size = width * 0.38;
+const item_spacing = (width - item_size) / 2;
 
 const HomeScreen: React.FC = () => {
   const scrollX = useRef(new Animated.Value(0)).current;
