12e36db1344ace9032091dd9b0a914ee0cb858a5
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-16 19:16:17 +0100

    feat: added CountdownScreen

---
+++ src/screens/CountdownScreen.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const CountdownScreen = () => {
+  return (
+    <View>
+      <Text>CountdownScreen</Text>
+    </View>
+  );
+};
+
+export default CountdownScreen;
