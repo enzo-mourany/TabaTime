9385d925d18e48aa984727c27561974ed3ffeeaf
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-16 19:50:51 +0100

    feat: add navigation

--- App.tsx
+++ App.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
 import {DurationProvider} from './src/states/DurationProvider';
 
 import HomeScreen from './src/screens/HomeScreen';
 
+const Stack = createStackNavigator();
+
 const App = () => {
   return (
     <DurationProvider>
-      <SafeAreaView style={styles.container}>
-        <HomeScreen />
-      </SafeAreaView>
+      <NavigationContainer independent={true}>
+        <Stack.Navigator
+          initialRouteName="Home"
+          screenOptions={{
+            gestureEnable: true,
+            gestureDirection: 'horizontal',
+            headerMode: 'float',
+          }}
+          animation="fade">
+          <Stack.Screen
+            name="HomeScreen"
+            component={HomeScreen}
+            options={{
+              title: ' ',
+              headerShown: false,
+              headerStyle: {
+                backgroundColor: '#000000',
+              },
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </DurationProvider>
   );
 };
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#222222',
-  },
-});
--- src/screens/CountdownScreen.tsx
+++ src/screens/CountdownScreen.tsx
@@ -1,7 +1,7 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
-const CountdownScreen = () => {
+const CountdownScreen: React.FC = () => {
   return (
     <View>
       <Text>CountdownScreen</Text>
