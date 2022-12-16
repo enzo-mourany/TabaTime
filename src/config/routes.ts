ab3ad4296e3aca273f8de71786aa479b2bc90ed6
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-17 00:30:43 +0100

    feat: added routes

---
+++ src/config/routes.ts
@@ -0,0 +1,11 @@
+import {IRouteProps} from '../lib/RouteProp';
+import HomeScreen from '../screens/HomeScreen';
+
+const routes: IRouteProps[] = [
+  {
+    name: 'Home',
+    component: HomeScreen,
+  },
+];
+
+export default routes;
