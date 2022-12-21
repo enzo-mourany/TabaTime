7863bd594e3d103344a5bc9d50ba349e1087d77a
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-21 11:46:03 +0100

    feat: add style

--- src/screens/CountdownScreen.tsx
+++ src/screens/CountdownScreen.tsx
@@ -1,4 +1,10 @@
-import {Text, SafeAreaView, TouchableOpacity} from 'react-native';
+import {
+  Text,
+  SafeAreaView,
+  TouchableOpacity,
+  StyleSheet,
+  View,
+} from 'react-native';
 import React, {useContext, useEffect, useState} from 'react';
 
 import {DurationContext} from '../states/DurationProvider';
@@ -37,16 +43,62 @@ const CountdownScreen: React.FC = () => {
   };
 
   return (
-    <SafeAreaView>
-      <Text>{remainingTime}</Text>
-      <TouchableOpacity onPress={handleStart}>
-        <Text>{isRunning ? 'Pause' : 'Start'}</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={handleReset}>
-        <Text>Reset</Text>
-      </TouchableOpacity>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.timer}>
+        <Text style={styles.textTimer}>{remainingTime}</Text>
+      </View>
+      <View style={styles.buttons}>
+        <TouchableOpacity onPress={handleStart} style={styles.startButton}>
+          <Text>{isRunning ? 'Pause' : 'Start'}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={handleReset} style={styles.resetButton}>
+          <Text>Reset</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default CountdownScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timer: {
+    backgroundColor: 'white',
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  textTimer: {
+    fontSize: 100,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    width: '100%',
+    marginTop: 50,
+  },
+  startButton: {
+    backgroundColor: 'white',
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  resetButton: {
+    backgroundColor: 'white',
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
