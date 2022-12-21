e8f40c45024e58d2809aa71ce20296a7b71d1240
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-21 11:06:04 +0100

    feat: add remainingTime

--- src/screens/CountdownScreen.tsx
+++ src/screens/CountdownScreen.tsx
@@ -1,7 +1,15 @@
 import {View, Text} from 'react-native';
 import React from 'react';
 
+import {DurationContext} from '../states/DurationProvider';
+
 const CountdownScreen: React.FC = () => {
+  const {durationExercise, durationRest} = React.useContext(DurationContext);
+
+  const [isRunning, setIsRunning] = React.useState(false);
+  const [isExercise, setIsExercise] = React.useState(true);
+  const [remaintingTime, setRemaintingTime] = React.useState(durationExercise);
+
   return (
     <View>
       <Text>CountdownScreen</Text>
