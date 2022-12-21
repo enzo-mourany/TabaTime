55b8bd4c478f623c12c0f21d5350ab6dbf7069f4
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-21 18:20:51 +0100

    feat: add colors

--- src/styles/Styles.tsx
+++ src/styles/Styles.tsx
@@ -1,8 +1,10 @@
-type colors = 'black' | 'white' | 'grey' | 'darkGrey';
+type colors = 'black' | 'white' | 'gray' | 'darkGray' | 'blue' | 'cyan';
 
 export const Colors: Record<colors, string> = {
-  black: '#000000',
+  black: '#2d2f41',
   white: '#ffffff',
-  grey: '#7D86A9',
-  darkGrey: '#59607a',
+  gray: '#b3b3b3',
+  darkGray: '#4d4d4d',
+  blue: '#6356ff',
+  cyan: '#60cdff',
 };
