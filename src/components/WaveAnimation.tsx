508effcc1a1831131f1cc4b8aa769a90d0d947ee
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-22 10:47:30 +0100

    feat: add wave animation component

---
+++ src/components/WaveAnimation.tsx
@@ -0,0 +1,21 @@
+import React, {useEffect, useRef} from 'react';
+import {Animated, Easing, StyleSheet, View} from 'react-native';
+
+import Svg, {
+  Circle,
+  LinearGradient,
+  Path,
+  RadialGradient,
+  Stop,
+} from 'react-native-svg';
+import {Colors} from '../../styles/Styles';
+
+const WaveAnimation = () => {
+  return (
+    <View>
+      <Text>WaveAnimation</Text>
+    </View>
+  );
+};
+
+export default WaveAnimation;
