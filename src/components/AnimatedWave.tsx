368d8bdacb8a0b2eb937b22d755dee2393a3fc8f
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-22 11:09:04 +0100

    feat: add first circle animation

--- src/components/AnimatedWave.tsx
+++ src/components/AnimatedWave.tsx
@@ -1,24 +1,30 @@
-import React, {useRef} from 'react';
-import {View, StyleSheet, Animated} from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import {View, StyleSheet, Animated, Easing} from 'react-native';
+
+import {Colors} from '../styles/Styles';
 
 const AnimatedWave = () => {
   const scale = useRef(new Animated.Value(1)).current;
 
-  const growAnimation = () => {
-    Animated.timing(scale, {
-      toValue: 1.5,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start(() => shrinkAnimation());
-  };
-
-  const shrinkAnimation = () => {
-    Animated.timing(scale, {
-      toValue: 1,
-      duration: 1000,
-      useNativeDriver: true,
-    }).start(() => growAnimation());
-  };
+  useEffect(() => {
+    const anim = Animated.loop(
+      Animated.sequence([
+        Animated.timing(scale, {
+          toValue: 3,
+          duration: 1000,
+          useNativeDriver: true,
+          easing: Easing.ease,
+        }),
+        Animated.timing(scale, {
+          toValue: 1,
+          duration: 1000,
+          useNativeDriver: true,
+        }),
+      ]),
+    );
+    anim.start();
+    return () => anim.stop();
+  }, [scale]);
 
   return (
     <View style={styles.container}>
@@ -37,7 +43,8 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
-    backgroundColor: 'red',
+    backgroundColor: Colors.blue,
+    opacity: 0.5,
   },
 });
 
