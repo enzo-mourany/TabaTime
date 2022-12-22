e29958a277a86dc6999bcb07413079b09af52153
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-22 11:27:22 +0100

    feat: finish circles animations

--- src/components/AnimatedWave.tsx
+++ src/components/AnimatedWave.tsx
@@ -4,31 +4,81 @@ import {View, StyleSheet, Animated, Easing} from 'react-native';
 import {Colors} from '../styles/Styles';
 
 const AnimatedWave = () => {
-  const scale = useRef(new Animated.Value(1)).current;
+  const firstCircleScale = useRef(new Animated.Value(3)).current;
+  const secondCircleScale = useRef(new Animated.Value(4.5)).current;
+  const thirdCircleScale = useRef(new Animated.Value(6)).current;
 
   useEffect(() => {
     const anim = Animated.loop(
       Animated.sequence([
-        Animated.timing(scale, {
-          toValue: 3,
-          duration: 1000,
+        Animated.timing(firstCircleScale, {
+          toValue: 3.5,
+          duration: 2000,
           useNativeDriver: true,
           easing: Easing.ease,
         }),
-        Animated.timing(scale, {
-          toValue: 1,
-          duration: 1000,
+        Animated.timing(firstCircleScale, {
+          toValue: 3,
+          duration: 2000,
           useNativeDriver: true,
         }),
       ]),
     );
     anim.start();
     return () => anim.stop();
-  }, [scale]);
+  }, [firstCircleScale]);
+
+  useEffect(() => {
+    const anim = Animated.loop(
+      Animated.sequence([
+        Animated.timing(secondCircleScale, {
+          toValue: 5,
+          duration: 3000,
+          useNativeDriver: true,
+          easing: Easing.ease,
+        }),
+        Animated.timing(secondCircleScale, {
+          toValue: 4.5,
+          duration: 3000,
+          useNativeDriver: true,
+        }),
+      ]),
+    );
+    anim.start();
+    return () => anim.stop();
+  }, [secondCircleScale]);
+
+  useEffect(() => {
+    const anim = Animated.loop(
+      Animated.sequence([
+        Animated.timing(thirdCircleScale, {
+          toValue: 6.5,
+          duration: 4000,
+          useNativeDriver: true,
+          easing: Easing.ease,
+        }),
+        Animated.timing(thirdCircleScale, {
+          toValue: 6,
+          duration: 4000,
+          useNativeDriver: true,
+        }),
+      ]),
+    );
+    anim.start();
+    return () => anim.stop();
+  }, [thirdCircleScale]);
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.circle, {transform: [{scale}]}]} />
+      <Animated.View
+        style={[styles.circle, {transform: [{scale: firstCircleScale}]}]}
+      />
+      <Animated.View
+        style={[styles.circle, {transform: [{scale: secondCircleScale}]}]}
+      />
+      <Animated.View
+        style={[styles.circle, {transform: [{scale: thirdCircleScale}]}]}
+      />
     </View>
   );
 };
@@ -43,8 +93,9 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
-    backgroundColor: Colors.blue,
-    opacity: 0.5,
+    backgroundColor: Colors.white,
+    opacity: 0.1,
+    position: 'absolute',
   },
 });
 
