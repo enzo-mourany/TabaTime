e6e46ba17c4e01ba8df2068e975913db8af1e4e1
Enzo Mourany <enzo.mourany@outlook.com>
2022-12-23 10:56:34 +0100

    feat: add circular progress bar animation

--- src/components/CircularProgress.tsx
+++ src/components/CircularProgress.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React, {useRef, useEffect} from 'react';
+import {View, StyleSheet, Animated} from 'react-native';
 import Svg, {Circle} from 'react-native-svg';
 
 import {Colors} from '../styles/Styles';
@@ -13,16 +13,46 @@ const CircularProgress: React.FC<CircularProgressProps> = ({
   remainingTime,
   initialValue,
 }) => {
-  const size = 280;
-  const strokeWidth = 3;
-  const radius = (size - strokeWidth) / 2;
-  const circum = radius * 2 * Math.PI;
-  const svgProgress = 100 - (remainingTime / initialValue) * 100;
+  const size: number = 280;
+  const strokeWidth: number = 3;
+  const radius: number = (size - strokeWidth) / 2;
+  const circum: number = radius * 2 * Math.PI;
+  const svgProgress: number = 100 - (remainingTime / initialValue) * 100;
+
+  const progressAnimation = useRef(new Animated.Value(0)).current;
+  const progressRef = useRef(null);
+
+  const animation = (toValue: number) => {
+    return Animated.timing(progressAnimation, {
+      toValue,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  useEffect(() => {
+    animation(svgProgress);
+  }, [svgProgress]);
+
+  useEffect(() => {
+    progressAnimation.addListener(
+      v => {
+        const strokeDashoffset = radius * Math.PI * 2 * (v.value / 100);
+        if (progressRef?.current) {
+          progressRef.current.setNativeProps({
+            strokeDashoffset,
+          });
+        }
+      },
+      [progressAnimation],
+    );
+  }, [progressAnimation, radius]);
 
   return (
     <View style={styles.container}>
       <Svg height="280" width="280">
         <Circle
+          ref={progressRef}
           stroke={Colors.white}
           fill="none"
           cx={size / 2}
