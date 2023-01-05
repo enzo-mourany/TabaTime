8d5bd5455f2ff86b21aae05e7630abd3300455f1
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-05 12:27:49 +0100

    fix: resolve import bug name

--- src/components/TabataRunBox.tsx
+++ src/components/TabataRunBox.tsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import {View, StyleSheet, Dimensions} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Animated,
+  Easing,
+} from 'react-native';
 
-import ScrollExerciseDuration from './ScrollRestDuration';
+import ScrollExerciseDuration from './ScrollExerciseDuration';
 import ScrollRestDuration from './ScrollRestDuration';
+import NavigateButton from '../components/NavigateButton';
 
 import {Colors} from '../styles/Styles';
 
 const {width, height} = Dimensions.get('window');
 
 const TabataRunBox = () => {
+  const [open, setOpen] = useState(false);
+
+  const scaleValue = useRef(new Animated.Value(0.6)).current;
+
+  const handleOpen = () => {
+    setOpen(!open);
+    console.log(open);
+
+    Animated.timing(scaleValue, {
+      toValue: open ? 0.6 : 1,
+      duration: 250,
+      easing: Easing.linear,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.scrollView}>
-        <ScrollExerciseDuration />
-        <ScrollRestDuration />
-      </View>
-    </View>
+    <Animated.View
+      style={{...styles.container, transform: [{scaleY: scaleValue}]}}>
+      <TouchableOpacity
+        style={styles.container}
+        activeOpacity={0.8}
+        onPress={handleOpen}>
+        <View style={styles.scrollView}>
+          <ScrollExerciseDuration />
+          <ScrollRestDuration />
+        </View>
+        <NavigateButton />
+      </TouchableOpacity>
+    </Animated.View>
   );
 };
 
@@ -23,16 +54,18 @@ export default TabataRunBox;
 
 const styles = StyleSheet.create({
   container: {
-    //flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     width: width * 0.9,
-    height: height * 0.5,
+    height: height * 0.6,
     backgroundColor: Colors.darkGray,
     borderRadius: 25,
   },
   scrollView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     position: 'relative',
-    width: width,
+    //width: width,
   },
 });
