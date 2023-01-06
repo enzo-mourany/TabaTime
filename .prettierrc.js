510b1c8bf82a858b5a01087059348a0bef10473c
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-06 11:58:05 +0100

    feat: add new rules

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,17 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  useTabs: true,
+  tabWidth: 2,
+  semi: true,
+  printWidth: 120,
+  endOfLine: 'auto',
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      options: {
+        parser: 'typescript',
+      },
+    },
+  ],
 };
