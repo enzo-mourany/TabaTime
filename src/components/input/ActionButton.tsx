9f74d4217ce1e02062e632e43b60588dc6ffc30a
Enzo Mourany <enzo.mourany@outlook.com>
2023-01-06 13:48:54 +0100

    feat: use navigate button

--- src/components/input/ActionButton.tsx
+++ src/components/input/ActionButton.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, Dimensions } from 'react-native';
 import { Colors } from '../../styles/Styles';
+
+const { width } = Dimensions.get('window');
 
 interface ActionButtonProps {
     onPress: () => void;
@@ -16,10 +18,7 @@ const ActionButton: React.FC<ActionButtonProps> = ({ onPress, buttonText, style,
       ...styles.container,
       ...style,
     }}>
-      <Text style={{
-        letterSpacing: 1,
-        textAlign: 'center',
-      }} adjustsFontSizeToFit numberOfLines={2}>
+      <Text style={styles.buttonText} adjustsFontSizeToFit>
         {buttonText}
       </Text>
   </TouchableOpacity>
@@ -29,10 +28,20 @@ export default ActionButton;
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
+    width: width * 0.8,
+    height: 60,
     borderRadius: 18,
     backgroundColor: Colors.cyan,
     shadowRadius: 13,
     shadowOffset: { width: 0, height: 8 },
     shadowColor: Colors.cyan,
+  },
+  buttonText: {
+    color: Colors.black,
+    fontSize: 19,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    letterSpacing: 1,
   },
 });
--- src/screens/SetupTabataScreen.tsx
+++ src/screens/SetupTabataScreen.tsx
@@ -1,16 +1,27 @@
 import { SafeAreaView, StyleSheet } from 'react-native';
 import React from 'react';
+import { NavigationProp } from '@react-navigation/native';
 
 import ScrollExerciseDuration from '../components/ScrollExerciseDuration';
 import ScrollRestDuration from '../components/ScrollRestDuration';
 import ScrollRoundsNumber from '../components/ScrollRoundsNumber';
 
-const SetupTabataScreen = () => {
+import ActionButton from '../components/input/ActionButton';
+
+interface SetupTabataScreenProps {
+  navigation: NavigationProp<any>;
+}
+
+const SetupTabataScreen: React.FC<SetupTabataScreenProps> = ({ navigation }) => {
   return (
     <SafeAreaView style={styles.container}>
       <ScrollExerciseDuration />
       <ScrollRestDuration />
       <ScrollRoundsNumber />
+      <ActionButton
+        onPress={() => navigation.navigate('Countdown')}
+        buttonText="Start Tabata"
+      />
     </SafeAreaView>
   )
 };
